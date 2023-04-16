696d8e5bff67a6e5dd9223655e2358661fa5f5a6
Tomtefa <91485182+Tomtefa@users.noreply.github.com>
2023-04-16 19:51:30 +0200

    under testing - download at your own risk!
    Co-authored-by: Asif Rezai <AlexMRTY@users.noreply.github.com>

--- src/server.c
+++ src/server.c
@@ -1,21 +1,26 @@
-#include <stdio.h>
-#include <stdbool.h>
 #include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 #include "SDL2/SDL.h"
 #include "SDL2/SDL_image.h"
 #include "SDL2/SDL_mixer.h"
 #include "SDL2/SDL_net.h"
-
 #include "player.h"
+#include "world.h"
 
 #define SERVER_PORT 12345
 #define MAX_PLAYERS 4
+#define WINDOW_WIDTH 1024
+#define WINDOW_HEIGHT 1024
+#define PLAYER_HIGHT 32
+#define PLAYER_WIDTH 32
 
-int main(int argc, char **argv)
-{
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-    {
+bool collisionDetection(int dx, int dy);
+bool collisionWithMap(int dx, int dy);
+
+int main(int argc, char **argv) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         printf("Error: %s\n", SDL_GetError());
         return 1;
     }
@@ -23,8 +28,7 @@ int main(int argc, char **argv)
     SDLNet_Init();
 
     UDPsocket server_socket = SDLNet_UDP_Open(SERVER_PORT);
-    if (server_socket == NULL)
-    {
+    if (server_socket == NULL) {
         printf("Failed to open server socket: %s\n", SDLNet_GetError());
         return 1;
     }
@@ -34,24 +38,21 @@ int main(int argc, char **argv)
     Player players[MAX_PLAYERS];
     int number_of_players = 0;
 
-    while (1)
-    {
+    while (1) {
         // Receive player updates and join requests
-        while (SDLNet_UDP_Recv(server_socket, recieve))
-        {
-            printf("UDP Packet incoming\n");
-            printf("\tData:    %s\n", (char *)recieve->data);
-            printf("\tAddress: %x %x\n", recieve->address.host, recieve->address.port);
+        while (SDLNet_UDP_Recv(server_socket, recieve)) {
+            // printf("UDP Packet incoming\n");
+            // printf("\tData:    %s\n", (char *)recieve->data);
+            // printf("\tAddress: %x %x\n", recieve->address.host, recieve->address.port);
 
             int dx, dy, player_id;
             sscanf((char *)recieve->data, "%d %d %d", &dx, &dy, &player_id);
 
-            if (strcmp((char *)recieve->data, "join_request") == 0 && number_of_players < MAX_PLAYERS)
-            {
+            if (strcmp((char *)recieve->data, "join_request") == 0 && number_of_players < MAX_PLAYERS) {
                 Player player = {number_of_players, {100, 100, 50, 50}, recieve->address};
                 players[number_of_players] = player;
                 number_of_players++;
-                printf("Player joined with ID %d\n", player.id);
+                // printf("Player joined with ID %d\n", player.id);
 
                 packet->address.host = player.address.host;
                 packet->address.port = player.address.port;
@@ -62,34 +63,35 @@ int main(int argc, char **argv)
                 memset(packet->data, 0, sizeof(packet->data));
             }
 
-            for (int i = 0; i < number_of_players; i++)
-            {
-                if (players[i].id == player_id)
-                {
+            for (int i = 0; i < number_of_players; i++) {
+                if (players[i].id == player_id) {
                     // Update existing player's position
+                    if (collisionDetection(players[i].rect.x, players[i].rect.y)) {
+                        printf("**************************Krock!\n");
+                    } else {
+                        printf("Ingen krock!\n");
+                    }
+
                     players[i].rect.x = dx;
                     players[i].rect.y = dy;
-                    printf("Update\n");
+                    // printf("Update\n");
                     break;
                 }
             }
 
             // Send player updates to all clients
-            for (int i = 0; i < number_of_players; i++)
-            {
+            for (int i = 0; i < number_of_players; i++) {
                 UDPpacket *update_packet = SDLNet_AllocPacket(512);
-                update_packet->address.host = players[i].address.host; // set host to the client address
-                update_packet->address.port = players[i].address.port; // set port to the client address
+                update_packet->address.host = players[i].address.host;  // set host to the client address
+                update_packet->address.port = players[i].address.port;  // set port to the client address
 
-                for (int j = 0; j < number_of_players; j++)
-                {
-                    if (i != j)
-                    {
-                        sprintf((char *)update_packet->data, "player_data %d %d %d", players[j].rect.x, players[j].rect.y, players[j].id);
+                for (int j = 0; j < number_of_players; j++) {
+                    if (i != j) {
+                        // sprintf((char *)update_packet->data, "player_data %d %d %d", players[j].rect.x, players[j].rect.y, players[j].id);
                         update_packet->len = strlen((char *)update_packet->data) + 1;
 
                         SDLNet_UDP_Send(server_socket, -1, update_packet);
-                        printf("Sending data to player %d\n", players[i].id);
+                        // printf("Sending data to player %d\n", players[i].id);
                     }
                 }
 
@@ -104,4 +106,25 @@ int main(int argc, char **argv)
     SDL_Quit();
 
     return 0;
+}
+bool collisionDetection(int dx, int dy) {
+    int collison = 0;
+    collison += collisionWithMap(dx, dy);
+    collison += collisionWithMap(dx + PLAYER_WIDTH, dy);
+    collison += collisionWithMap(dx, dy + PLAYER_HIGHT);
+    collison += collisionWithMap(dx + PLAYER_WIDTH, dy + PLAYER_HIGHT);
+    return collison > 0;
+}
+bool collisionWithMap(int dx, int dy) {
+    // check the corners (all 4)
+    // För långt till vänster och ner.
+    int collumn = getCol(dx);
+    int row = getRow(dy);
+    printf(" X:%d Y:%d , Col:%d, Row:%d \n", dx, dy, collumn, row);
+
+    if (getTileGrid(collumn, row) == 7) {
+        printf("%d %d\n", dx, dy);  // TEST
+        return false;
+    }
+    return true;
 }
