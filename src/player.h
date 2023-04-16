c672a04a4dfff17d9a6fef6d49d07b850ec4a38b
faizaljendi1 <infinity-02@hotmail.com>
2023-04-16 23:43:38 +0200

    update/player

--- src/player.h
+++ src/player.h
@@ -1,6 +1,7 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 #include "SDL2/SDL.h"
+#include "SDL2/SDL_net.h"
 #include <stdbool.h>
 
 typedef struct
@@ -8,6 +9,10 @@ typedef struct
     int id;
     SDL_Rect rect;
     IPaddress address;
+    bool isAlive;
+    int numberOfPoints;
+    char pngName[50];
+    int movement;
 } Player;
 
 void initializePlayer();
