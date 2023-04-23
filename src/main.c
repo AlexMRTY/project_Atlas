7fd52491ec644872b1038b7f6d4005903bcf5592
Nancy <79465924+NancyIbrahim@users.noreply.github.com>
2023-04-23 21:27:15 +0200

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -2,31 +2,15 @@
 #include <SDL2/SDL_image.h>
 #include <stdbool.h>
 #include <stdio.h>
-
+#include <SDL2/SDL_mixer.h>
 #include "SDL2/SDL_net.h"
+
+#include "globalConst.h"
 #include "player.h"
 #include "world.h"
-
-#define SPEED 200 // 100
-#define WINDOW_WIDTH 1024
-#define WINDOW_HEIGHT 1024
-#define PLAYER_MOVE_SPEED 32
-#define PLAYER_HIGHT 32
-#define PLAYER_WIDTH 32
-
-#define NUM_SUBTEXTURES 4
-#define TEXTURE_WIDTH 128
-#define TEXTURE_HEIGHT 128
-#define SUBTEXTURE_WIDTH 32
-#define SUBTEXTURE_HEIGHT 32
-
-#define SERVER_IP "127.0.0.1"
-#define SERVER_PORT 12345
-#define MAX_PLAYERS 4
-
-void loadTiles(SDL_Renderer *gRenderer, SDL_Texture **mTiles, SDL_Rect gTiles[]);
-void renderMap(SDL_Renderer *gRenderer, SDL_Texture *mTiles, SDL_Rect gTiles[]);
-void renderPlayers(SDL_Renderer *pRenderer, SDL_Texture **pTextures, SDL_Rect *subtextures, int num_subtextures, Player *players, int num_players, Player me);
+#include "events.h"
+#include "render.h"
+#include "client.h"
 
 int main(int argv, char **args)
 {
@@ -58,11 +42,16 @@ int main(int argv, char **args)
         return 1;
     }
 
-    // const int NUM_SUBTEXTURES = 4;
-    // const int TEXTURE_WIDTH = 128;
-    // const int TEXTURE_HEIGHT = 128;
-    // const int SUBTEXTURE_WIDTH = 32;
-    // const int SUBTEXTURE_HEIGHT = 32;
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
+    {
+        printf("SDL2_mixer could not initialize! SDL2_mixer Error: %s\n", Mix_GetError());
+    }
+
+    Mix_Music *music = Mix_LoadMUS("resources/sfx_step_rock_l.mp3");
+    if (music == NULL)
+    {
+        printf("Failed to load music! SDL2_mixer Error: %s\n", Mix_GetError());
+    }
 
     SDL_Rect subtextures[NUM_SUBTEXTURES];
 
@@ -115,7 +104,7 @@ int main(int argv, char **args)
     packet->address.port = server_address.port;
 
     int number_of_player = 0;
-    Player players[MAX_PLAYERS];
+    Player players[MAX_PLAYERS] = {0};
     Player me;
     bool joinedServer = false;
     bool quit = false;
@@ -132,247 +121,33 @@ int main(int argv, char **args)
         // Receive updates from other players
         while (SDLNet_UDP_Recv(client_socket, recieve))
         {
-            printf("UDP Packet from server\n");
-            printf("\tData:    %s\n", (char *)recieve->data);
-            printf("\tAddress: %x %x\n", recieve->address.host, recieve->address.port);
-
-            int x, y, id, nrOfpoints, movement;
-            int x2, y2, id2, nrOfpoints_2, movement_2;
-            if (!joinedServer && sscanf((char *)recieve->data, "join_accept %d %d %d %d %d", &x, &y, &id, &nrOfpoints, &movement) == 5 && number_of_player <= MAX_PLAYERS)
-            {
-                printf("Joined server!\n");
-                Player player = {id, {x, y, PLAYER_WIDTH, PLAYER_HIGHT}, nrOfpoints, movement};
-                players[number_of_player] = player;
-                me = player;
-                number_of_player++;
-                joinedServer = true;
-            }
-            else if (sscanf((char *)recieve->data, "player_data %d %d %d %d %d", &x2, &y2, &id2, &nrOfpoints_2, &movement_2) == 5)
-            {
-                printf("data from other players\n");
-                int index = -1;
-
-                if (number_of_player <= MAX_PLAYERS)
-                {
-                    bool found = false;
-                    for (int i = 0; i < number_of_player; i++)
-                    {
-                        if (players[i].id == id2)
-                        {
-                            found = true;
-                            index = i;
-                            break;
-                        }
-                    }
-                    if (!found)
-                    {
-                        Player player = {id2, {x2, y2, PLAYER_WIDTH, PLAYER_HIGHT}, nrOfpoints_2, movement_2};
-                        players[number_of_player] = player;
-                        number_of_player++;
-                        printf("Added player with ID %d\n", id2);
-                    }
-                    else
-                    {
-                        players[index].rect.x = x2;
-                        players[index].rect.y = y2;
-                        players[index].movement = movement_2;
-                        players[index].numberOfPoints = nrOfpoints_2;
-                        printf("Player Already exist\n");
-                    }
-                }
-            }
-            else
-            {
-                printf("Unknown message received: %s\n", (char *)packet->data);
-            }
+            receive_updates(&client_socket, recieve, players, &me, &number_of_player, &joinedServer);
         }
 
-        // Get player input
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
-            if (event.type == SDL_QUIT)
-            {
-                quit = true;
-            }
-            else if (event.type == SDL_KEYDOWN)
-            {
-                switch (event.key.keysym.sym)
-                {
-                case SDLK_UP:
-                    if (me.rect.y - PLAYER_MOVE_SPEED >= 0)
-                    {
-                        me.rect.y -= PLAYER_MOVE_SPEED;
-                    }
-                    else
-                    {
-                        me.rect.y = 0;
-                    }
-                    me.movement = 1;
-                    break;
-                case SDLK_DOWN:
-                    if (me.rect.y + PLAYER_MOVE_SPEED <= WINDOW_HEIGHT - PLAYER_HIGHT)
-                    {
-                        me.rect.y += PLAYER_MOVE_SPEED;
-                    }
-                    else
-                    {
-                        me.rect.y = WINDOW_HEIGHT - PLAYER_HIGHT;
-                    }
-                    me.movement = 2;
-                    break;
-                case SDLK_LEFT:
-                    if (me.rect.x - PLAYER_MOVE_SPEED >= 0)
-                    {
-                        me.rect.x -= PLAYER_MOVE_SPEED;
-                    }
-                    else
-                    {
-                        me.rect.x = 0;
-                    }
-                    me.movement = 3;
-                    break;
-                case SDLK_RIGHT:
-                    if (me.rect.x + PLAYER_MOVE_SPEED <= WINDOW_WIDTH - PLAYER_WIDTH)
-                    {
-                        me.rect.x += PLAYER_MOVE_SPEED;
-                    }
-                    else
-                    {
-                        me.rect.x = WINDOW_WIDTH - PLAYER_WIDTH;
-                    }
-                    me.movement = 4;
-                    break;
-                }
-            }
-        }
+        handleEvents(&me.rect, &me.movement, &quit); // Handles quit and movement events
+
         if (joinedServer)
         {
-            // Send player position update to server
-            sprintf((char *)packet->data, "%d %d %d %d %d", me.rect.x, me.rect.y, me.id, me.numberOfPoints, me.movement);
-            packet->len = strlen((char *)packet->data) + 1;
-            SDLNet_UDP_Send(client_socket, -1, packet);
-
-            // printf("Sending data\n");        // Commented out during testing
-            // printf("MY ID: %d\n", me.id);    // Commented out during testing
+            // Transmit my data to others
+            transmitData(&me, packet, &client_socket);
 
             SDL_SetRenderDrawColor(pRenderer, 0, 0, 0, 255);
             SDL_RenderClear(pRenderer);
+
             // Render background
             renderMap(pRenderer, tTiles, gTiles);
-            renderPlayers(pRenderer, client_textures, subtextures, NUM_SUBTEXTURES, players, number_of_player, me);
+            printf("music pointer value: %p\n", music);
+            renderPlayers(pRenderer, client_textures, subtextures, NUM_SUBTEXTURES, players, number_of_player, me, music);
         }
     }
 
-    //   SDL_DestroyTexture(pTexture);
+    Mix_CloseAudio();
     SDL_DestroyRenderer(pRenderer);
     SDL_DestroyWindow(pWindow);
-
     SDLNet_FreePacket(packet);
     SDLNet_UDP_Close(client_socket);
     SDLNet_Quit();
     SDL_Quit();
 
     return 0;
-}
-
-void renderPlayers(SDL_Renderer *pRenderer, SDL_Texture **pTexture, SDL_Rect *subtextures, int num_subtextures, Player *players, int num_players, Player me)
-{
-    // Change current subtexture based on player movement
-    SDL_Rect currentSubtexture;
-    if (me.movement == 1)
-    {
-        currentSubtexture = subtextures[0];
-    }
-    else if (me.movement == 3)
-    {
-        currentSubtexture = subtextures[1];
-        SDL_Texture *playerTexture = pTexture[me.id];
-        SDL_RendererFlip flip = SDL_FLIP_HORIZONTAL;
-        SDL_RenderCopyEx(pRenderer, playerTexture, &currentSubtexture, &me.rect, 0, NULL, flip);
-    }
-    else if (me.movement == 2)
-    {
-        currentSubtexture = subtextures[2];
-    }
-    else if (me.movement == 4)
-    {
-        currentSubtexture = subtextures[1];
-    }
-
-    // Render current player with correct subtexture
-    SDL_Texture *playerTexture = pTexture[me.id];
-    SDL_RenderCopyEx(pRenderer, playerTexture, &currentSubtexture, &me.rect, 0, NULL, SDL_FLIP_NONE);
-
-    // Render all other players
-    for (int i = 0; i < num_players; i++)
-    {
-        if (players[i].id != me.id)
-        {
-            if (players[i].movement == 1)
-            {
-                currentSubtexture = subtextures[0];
-            }
-            else if (players[i].movement == 3)
-            {
-                currentSubtexture = subtextures[1];
-                SDL_Texture *playerTexture = pTexture[players[i].id];
-                SDL_RendererFlip flip = SDL_FLIP_HORIZONTAL;
-                SDL_RenderCopyEx(pRenderer, playerTexture, &currentSubtexture, &players[i].rect, 0, NULL, flip);
-            }
-            else if (players[i].movement == 2)
-            {
-                currentSubtexture = subtextures[2];
-            }
-            else if (players[i].movement == 4)
-            {
-                currentSubtexture = subtextures[1];
-            }
-
-            // Render current player with correct subtexture
-            SDL_Texture *playerTexture = pTexture[players[i].id];
-            SDL_RenderCopyEx(pRenderer, playerTexture, &currentSubtexture, &players[i].rect, 0, NULL, SDL_FLIP_NONE);
-        }
-    }
-
-    SDL_RenderPresent(pRenderer);
-}
-
-void loadTiles(SDL_Renderer *gRenderer, SDL_Texture **mTiles, SDL_Rect gTiles[])
-{
-    SDL_Surface *gTilesSurface = IMG_Load("resources/tilemap.png");
-    if (!gTilesSurface)
-    {
-        printf("Error: %s\n", SDL_GetError());
-        SDL_DestroyRenderer(gRenderer);
-        // SDL_DestroyWindow(Window);
-        SDL_Quit();
-    }
-    *mTiles = SDL_CreateTextureFromSurface(gRenderer, gTilesSurface);
-    for (int i = 0; i < 8; i++)
-    {
-        gTiles[i].x = i * getTileWidth();
-        gTiles[i].y = 0;
-        gTiles[i].w = getTileWidth();
-        gTiles[i].h = getTileHeight();
-    }
-}
-
-void renderMap(SDL_Renderer *gRenderer, SDL_Texture *mTiles, SDL_Rect gTiles[])
-{
-    SDL_Rect position;
-    position.y = 0;
-    position.x = 0;
-    position.h = getTileHeight();
-    position.w = getTileWidth();
-
-    for (int i = 0; i < getTileColumns(); i++)
-    {
-        for (int j = 0; j < getTileRows(); j++)
-        {
-            position.y = i * getTileHeight();
-            position.x = j * getTileWidth();
-            SDL_RenderCopyEx(gRenderer, mTiles, &gTiles[getTileGrid(i, j)], &position, 0, NULL, SDL_FLIP_NONE);
-        }
-    }
 }
